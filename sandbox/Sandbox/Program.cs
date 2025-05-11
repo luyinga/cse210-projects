c33b404742235a33fa59e680e9c8d810b179937d
luyinga <lumbaleluyinga2018@gmail.com>
2025-05-11 22:11:20 +0200

    Finished all excercises

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,32 @@
 using System;
 
-class Program
+/*Console.WriteLine("Hello World! This is the Sandbox Project.");
+int number = 5;
+int number2 = number + 3;
+number = 8;
+
+Console.WriteLine($"My number is {number2}");
+
+Console.Write("What is your favorite color?");
+string color = Console.ReadLine();
+Console.WriteLine($"My favorite color is {color}");*/
+
+//Conditional statement and variables
+Console.Write("Enter number: ");
+int valueFromUser = int.Parse(Console.ReadLine());
+
+int x = valueFromUser;
+int y = 2;
+
+if (x > y)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
-    }
+    Console.WriteLine("Greater");
+}
+else if (x < y)
+{
+    Console.WriteLine("Less");
+}
+else
+{
+    Console.WriteLine("x is equal to y");
 }
