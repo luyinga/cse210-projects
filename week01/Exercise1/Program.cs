8b86406e4c41be8c1b196e7a5fe2a4dfc4ed8f9c
luyinga <lumbaleluyinga2018@gmail.com>
2025-05-11 15:29:20 +0200

    Finished excercise1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //Ask the user for their name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"your name is {firstName}, {lastName} {firstName}.");
     }
 }
