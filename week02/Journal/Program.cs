f03af9db6fe017fbfb1e14d378a53a1672ef05f1
luyinga <lumbaleluyinga2018@gmail.com>
2025-05-18 16:26:59 +0200

    Journal.txt added

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -6,11 +6,11 @@ class Program
 {
     static void Main(string[] args)
     {
-       
-       PromptGenerator  promptGenerator = new PromptGenerator();
-       Journal          journal = new Journal();
-       int              menuUserInput = 0;
-       List<string>     menu = new List<String>
+
+        PromptGenerator promptGenerator = new PromptGenerator();
+        Journal journal = new Journal();
+        int menuUserInput = 0;
+        List<string> menu = new List<String>
        {
           "Please select one of the following choices: ",
           "1. Write",
@@ -19,45 +19,45 @@ class Program
           "4. Save",
           "5. Quit",
           "What would you like to do? "
-       }
+       };
 
-       Console.WriteLine("Welcome to the journal program.");
+        Console.WriteLine("Welcome to the journal program.");
 
-       while (menuUserInput != 5)
-       {
-          foreach (string menuItem in menu)
-          {
-             Console.WriteLine(menuItem);
-          }
-          menuUserInput = int.Parse(Console.ReadLine());
+        while (menuUserInput != 5)
+        {
+            foreach (string menuItem in menu)
+            {
+                Console.WriteLine(menuItem);
+            }
+            menuUserInput = int.Parse(Console.ReadLine());
 
-          switch (menuUserInput)
-          {
-             case 1:
-                 string prompt = PromptGenerator.GetRandomPrompt();
-                 Console.WriteLine(prompt);
-                 Console.Write("> ");
-                 string response = Console.ReadLine();
-                 string date = DateTime.Now.Date.ToString("MMM dd, yyyy");
+            switch (menuUserInput)
+            {
+                case 1:
+                    string prompt = PromptGenerator.GetRandomPrompt();
+                    Console.WriteLine(prompt);
+                    Console.Write("> ");
+                    string response = Console.ReadLine();
+                    string date = DateTime.Now.Date.ToString("MMM dd, yyyy");
 
-                 Entry entry = new Entry(date, prompt, response);
-                 journal.AddEntry(entry);
-                 break;
-             case 2:
-                 journal.DisplayAll();
-                 break;
-             case 3:
-                 Console.WriteLine("Name of file to load?");
-                 string fileNameLoad = Console.ReadLine();
-                 journal.LoadFromFile(fileNameLoad);
-                 break;
-             case 4:
-                 Console.WriteLine("Name of file to save?");
-                 string fileNameSave = Console.ReadLine();
-                 journal.SaveToFile(fileNameSave);
+                    Entry entry = new Entry(date, prompt, response);
+                    journal.AddEntry(entry);
+                    break;
+                case 2:
+                    journal.DisplayAll();
+                    break;
+                case 3:
+                    Console.WriteLine("Name of file to load?");
+                    string fileNameLoad = Console.ReadLine();
+                    journal.LoadFromFile(fileNameLoad);
+                    break;
+                case 4:
+                    Console.WriteLine("Name of file to save?");
+                    string fileNameSave = Console.ReadLine();
+                    journal.SaveToFile(fileNameSave);
 
-                 break;          
-          }
-       }
+                    break;
+            }
+        }
     }
 }
