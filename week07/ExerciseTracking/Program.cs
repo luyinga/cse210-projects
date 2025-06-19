48142d8f2e433e378a596c6612f81812d05b8bea
luyinga <lumbaleluyinga2018@gmail.com>
2025-06-19 23:13:03 +0200

    Exercise Tracking added

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Exercise> exerciseList = new List<Exercise>();
+
+        Running running = new Running("June 19, 2025", 35, 5.8);
+        exerciseList.Add(running);
+
+        StationaryBicycles stationaryBicycles = new StationaryBicycles("June 22, 2025", 50, 20);
+        exerciseList.Add(stationaryBicycles);
+
+        Swimming swimming = new Swimming("June 24, 2025", 26, 26);
+        exerciseList.Add(swimming);
+
+        Console.WriteLine("Exercises:");
+        Console.WriteLine();
+
+        foreach (Exercise exercise in exerciseList)
+        {
+            Console.WriteLine(exercise.GetSummary());
+        }
     }
 }
